eb08f598d12d2304f6905298f5ccd77ee36c77a7
sir <sir@DESKTOP-KSQ6RIJ>
2022-04-27 12:02:48 +0530

    by prasad Sample 1

---
+++ Git_MultipleUser/src/Sample.java
@@ -0,0 +1,33 @@
+
+public class Sample
+{
+
+
+	public void add()
+	{
+		System.out.println("m1 go git");
+	}
+	
+	public void sub()
+	{
+		System.out.println("m2 go git");
+	}
+	
+	
+	
+	public static void main(String[] args) 
+	
+	{
+	
+		System.out.println("Hello Acc");
+		System.out.println("Hello Cjc");
+	
+	
+	
+	
+	Sample a=new Sample();
+	a.add();
+	a.sub();
+	}
+	
+}
